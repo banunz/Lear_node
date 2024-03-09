9ce920565e62f2ae280b3bf5ce57c8b0562711c1
Janu <banucres@gmail.com>
2024-03-09 14:04:04 +1300

    Adding the json parser file

--- filesystem.js
+++ filesystem.js
@@ -4,3 +4,15 @@ fse.readFile('C:/Users/banun/Dev/Lear_node/employee.txt', (err, data) => {
     if (err) throw err;
     console.log(data.toString());
   }); 
+
+  fse.readFile('C:/Users/banun/Dev/Lear_node/employee.json', (err,jsondata) => {
+    if (err) throw err;
+    console.log(jsondata.toString());
+  })
+
+  
+  fse.readFile('C:/Users/banun/Dev/Lear_node/employee.json', (err,jsondata) => {
+    if (err) throw err;
+    const jemplo=JSON.parse(jsondata)
+    console.log(jemplo);
+  })
